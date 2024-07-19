cbfc972deb8eeeae797ed59f852e03871421fbf8
Swayam Raj Singh <94851545+swayam0909@users.noreply.github.com>
2024-07-19 18:28:58 +0530

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,24 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MyToken is ERC20, Ownable {
-    constructor() ERC20("Ananya Sharma", "AS") {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+//owner 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+contract MyToken is ERC20, Ownable  {
+    constructor() ERC20("DogeCoin", "Dog") Ownable(address(uint160(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4)))
+ {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
-
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
-    }
-
     function transfer(address to, uint256 amount) public override returns (bool) {
         require(amount <= balanceOf(msg.sender), "Insufficient balance");
-        return super.transfer(to, amount);
-    }
-
-    // New function to get the token balance of an address
-    function getMyTokenBalance(address account) public view returns (uint256) {
-        return balanceOf(account);
+        return super.transfer(to,amount);
+}
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
     }
 }
